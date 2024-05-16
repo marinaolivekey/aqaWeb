a13f6376ba4f51dea0c9a4de6b760606862f80cc
Marina Kononova <marinakononova@mail.ru>
2024-05-17 01:19:41 +0400

    Selenium Test

--- src/test/java/ru/netology/order/orderAppTestV1.java
+++ src/test/java/ru/netology/order/orderAppTestV1.java
@@ -5,9 +5,15 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class orderAppTestV1 {
     private WebDriver driver;
@@ -34,10 +40,24 @@ public class orderAppTestV1 {
     }
 
     @Test
-    void shouldTestSomething(){
+    void shouldTestSomething() throws InterruptedException {
         //загрузить стр
         driver.get("http://0.0.0.0:9999/");
+
         //поиск элемента
+        WebElement form = driver.findElement(By.className("form"));
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Оксана Петрова");
+        elements.get(1).sendKeys("+79991234567");
+
         //взаимодействие с элементом
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+        Thread.sleep(50000);
+
+
     }
 }
