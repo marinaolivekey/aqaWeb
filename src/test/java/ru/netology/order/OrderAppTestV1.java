7e4fb7b10ce34c059d5e89dff3b1430d0408914f
Marina Kononova <marinakononova@mail.ru>
2024-05-20 15:27:00 +0400

    tests are corrected

--- src/test/java/ru/netology/order/OrderAppTestV1.java
+++ src/test/java/ru/netology/order/OrderAppTestV1.java
@@ -1,10 +1,7 @@
 package ru.netology.order;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class OrderAppTestV1 {
     private WebDriver driver;
@@ -22,7 +20,6 @@ public class OrderAppTestV1 {
     @BeforeAll
     public static void setupAll() {
         WebDriverManager.chromedriver().setup();
-        //System.setProperty("webdriver.chrome.driver","driver/chromedriver");
     }
 
     @BeforeEach
@@ -32,6 +29,8 @@ public class OrderAppTestV1 {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+
+        driver.get("http://localhost:9999/");
     }
 
     @AfterEach
@@ -41,9 +40,6 @@ public class OrderAppTestV1 {
 
     @Test
     void shouldTestHappyPath() throws InterruptedException {
-        //загрузить стр
-        driver.get("http://localhost:9999/");
-
         //поиск элемента
         WebElement form = driver.findElement(By.className("form"));
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Петров-Иванов");
@@ -55,32 +51,42 @@ public class OrderAppTestV1 {
         String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
 
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+        assertTrue(driver.findElement(By.cssSelector("[data-test-id=order-success]")).isDisplayed());
     }
 
     @Test
     void shouldShowInvalidInputIfNothingEntered() throws InterruptedException {
-        driver.get("http://localhost:9999/");
         WebElement form = driver.findElement(By.className("form"));
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("input__sub")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
 
         assertEquals("Поле обязательно для заполнения", text.trim());
     }
 
     @Test
-    void shouldShowInvalidInputIfNameEnteredPhoneNotCheckBoxNot() throws InterruptedException {
-        driver.get("http://localhost:9999/");
+    void shouldShowInvalidInputIfNameNotPhoneYesCheckBoxYes() throws InterruptedException {
+        WebElement form = driver.findElement(By.className("form"));
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79991234567");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
+
+        assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+
+    @Test
+    void shouldShowInvalidInputIfNameEnteredPhoneNotCheckBoxYes() throws InterruptedException {
         WebElement form = driver.findElement(By.className("form"));
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Петров-Иванов");
+        driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.xpath("//*[@id=\"root\"]/div/form/div[2]/span/span/span[3]")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText();
 
         assertEquals("Поле обязательно для заполнения", text.trim());
     }
 
     @Test
-    void shouldShowInvalidInputIfNameEnteredPhoneEnteredCheckBoxNot() throws InterruptedException {
-        driver.get("http://localhost:9999/");
+    void shouldShowInvalidInputIfNameEnteredPhoneYesCheckBoxNot() throws InterruptedException {
         WebElement form = driver.findElement(By.className("form"));
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Петров-Иванов");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79991234567");
@@ -92,42 +98,33 @@ public class OrderAppTestV1 {
 
     @Test
     void shouldShowInvalidIfNameEnglish() throws InterruptedException {
-        driver.get("http://localhost:9999/");
         WebElement form = driver.findElement(By.className("form"));
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Ivan petrov");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79991234567");
-
-        //взаимодействие с элементом
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("input__sub")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
 
         assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text);
     }
 
     @Test
     void shouldShowInvalidIfNameSpecialCharacters() throws InterruptedException {
-        driver.get("http://localhost:9999/");
         WebElement form = driver.findElement(By.className("form"));
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("!!! @@@");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79991234567");
-
-        //взаимодействие с элементом
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("input__sub")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
 
         assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text);
     }
 
     @Test
     void shouldAcceptIfSpaceBeforeName() throws InterruptedException {
-        driver.get("http://localhost:9999/");
         WebElement form = driver.findElement(By.className("form"));
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys(" Рита Маргарита");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79991234567");
-
-        //взаимодействие с элементом
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
         String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
@@ -136,48 +133,92 @@ public class OrderAppTestV1 {
         }
 
     @Test
-    void shouldAcceptIfSpaceOnly() throws InterruptedException {
-        driver.get("http://localhost:9999/");
+    void shouldShowInvalidIfSpaceOnly() throws InterruptedException {
         WebElement form = driver.findElement(By.className("form"));
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys(" ");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79991234567");
-
-        //взаимодействие с элементом
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("input__sub")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
 
         assertEquals("Поле обязательно для заполнения", text);
     }
 
     @Test
     void shouldShowInvalidIfNameNotFull() throws InterruptedException {
-        driver.get("http://localhost:9999/");
         WebElement form = driver.findElement(By.className("form"));
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Ольга");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79123456789");
-
-        //взаимодействие с элементом
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("input__sub")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
 
         assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text);
+    }
+
+    @Test
+    void shouldAcceptIfNamesIsOneLetter() throws InterruptedException {
+        WebElement form = driver.findElement(By.className("form"));
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Ю Ю");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79991234567");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    @Test
+    void shouldAcceptIfName100Letters() throws InterruptedException {
+        WebElement form = driver.findElement(By.className("form"));
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Напу Амо Хала Она Она Анека Вехи Вехи Она Хивеа Нена Вава Кехо Онка Кахе Хеа Леке Еа Она Ней Нана Ниа Кеко Оа Ога Ван Ика Ванао");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79123456789");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    @Test
+    void shouldShowInvalidIfNameStartWithDash() throws InterruptedException {
+        WebElement form = driver.findElement(By.className("form"));
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("-Ольга Петров");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79123456789");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
+
+        assertEquals("Имя и Фамилия указаные неверно", text);
+    }
+
+    @Test
+    void shouldShowInvalidIfNameEndsWithDash() throws InterruptedException {
+        WebElement form = driver.findElement(By.className("form"));
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Ольга Петров-");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79123456789");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
+
+        assertEquals("Имя и Фамилия указаные неверно", text);
+    }
+
+    @Test
+    void shouldShowInvalidIfPhoneIsNotCorrect() throws InterruptedException {
+        WebElement form = driver.findElement(By.className("form"));
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Петров-Долгих Ольга");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("Qwerty");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText();
+
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text);
     }
 }
 
 
-/* механизм поняла, нет времени дописать тесты
-из-за кучи тех моментов - то JNI ошибки, то Gradle JVM версии и тд, чего я только уже не исправила
-самм искала, время потеряла, нужно догнать программу
-
-дописала бы такие тесты
- + короткое имя Ю Ю
- + длинное имя 50 символов+50 символов
- + дефис перед именем
- + дефис после имени
- + тройное имя
-
+/*
  Тел:
  - буквы
  - спец символы
